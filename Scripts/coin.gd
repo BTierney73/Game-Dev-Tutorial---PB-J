7bdccbed913e0badab1870382641a91c22b872c5
Peter6524 <plott@ftstudent.org>
2026-03-10 10:13:40 -0400

    game manager

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
-
-
-
+@onready var game_manager = %GameManager
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Plus one coin!")
+	game_manager.add_point()
 	queue_free()
